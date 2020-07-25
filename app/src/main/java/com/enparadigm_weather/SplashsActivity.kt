a945a2713bb456d000bb93e40d961cdcbd9017a1
sarath <sarathkfs@gmail.com>
2020-07-25 18:40:24 +0530

    ->Splash Activity is added. ->working on Mainactivity

---
+++ app/src/main/java/com/enparadigm_weather/SplashsActivity.kt
@@ -0,0 +1,24 @@
+package com.enparadigm_weather
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+
+
+    }
+
+    companion object {
+        private const val SPLASH_TIME_OUT = 1000
+    }
+}
